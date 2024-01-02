c546913af914af40858c0ec8a8527f57565fda98
Isha Dubey <ishadubey36@gmail.com>
2024-01-02 19:55:45 +0530

    .

--- Spotify-Clone/src/main.js
+++ Spotify-Clone/src/main.js
@@ -1,22 +1,12 @@
 import './style.css'
 
+document.addEventListener("DOMContentLoaded" , () => {
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+  if(localStorage.getItem("accesstoken")){
+    window.location.href = "Dashboard/Dashboard.html"
+  }
+  else {
+    window.location.href = "Login/login.html"
 
-setupCounter(document.querySelector('#counter'))
+  }
+})
